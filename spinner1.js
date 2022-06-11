fc9774da5b749fd1eac3e2baa778c6f62a5a2b39
LIZXP <Peter.xli@outlook.com>
2022-06-10 18:54:35 -0600

    remove some notes

--- spinner1.js
+++ spinner1.js
@@ -11,31 +11,25 @@ setTimeout(() => {
 }, 500);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r\\   ");
 }, 700);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r|   ");
 }, 900);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r/   ");
 }, 1200);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r-   ");
 }, 1400);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r\\   ");
 }, 1600);
 
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
   process.stdout.write("\r|   ");
 }, 1800);
